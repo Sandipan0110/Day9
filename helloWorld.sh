5939d452b30ce6bd2d5ebd03e97aadf89ae1758b
Sandipan0110 <sandipanseal11@gmail.com>
2021-06-25 21:07:14 +0530

    checkEmp

--- helloWorld.sh
+++ helloWorld.sh
@@ -1,1 +1,13 @@
 echo "Welcome to empWage"
+
+isPresent=1;
+isabsent=0;
+randomChek=$((RANDOM%2));
+
+if [ $isPresent -eq $randomChek ];
+then
+        echo "emp is present"
+else
+        echo "emp is absent"
+
+fi
