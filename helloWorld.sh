9624b3d4f8d163a15f67c68ce265356e29781c6c
Sandipan0110 <sandipanseal11@gmail.com>
2021-06-26 20:34:30 +0530

    calculate working hrs

--- helloWorld.sh
+++ helloWorld.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -x
+#!/bin/bash -x
 
 echo "Welcome Message"
 
@@ -6,23 +6,29 @@ isPartTime=1;
 isFullTime=2;
 totalSalary=0;
 empRatePerHr=20;
-numWorkingDays=20;
+WorkingDays=20;
+maxHrsInMonth=100;
+totalEmpHrs=0;
+totalWorkingDays=0;
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+while [[ $totalEmpHrs -lt $maxHrsInMonth &&
+         $totalWorkingDays -lt $WorkingDays ]]
 do
+        ((totalWorkingDays++))
         empCheck=$((RANDOM%3));
-                case $empCheck in
-                        $isFullTime)
-                                empHrs=8
-                                ;;
-                        $isPartTime)
-                                empHrs=8
-                                ;;
-                        *)
-                                empHrs=0
-                                ;;
-                esac
-                salary=$(($empHrs*$empRatePerHr));
-                totalSalary=$(($totalSalary+$salary));
+        case $empCheck in
+                $isFullTime)
+                       empHrs=8
+                       ;;
+                $isPartTime)
+                       empHrs=8
+                       ;;
+                *)
+                       empHrs=0
+                       ;;
+         esac
+         totalEmpHrs=$(($totalEmpHrs+$empHrs));
+
 done
 
+totalMonthlySalary=$(($totalEmpHrs*$empRatePerHr));
