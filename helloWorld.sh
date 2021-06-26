10a949da05144cd6cbb4ad1473e9cff8d77f2916
Sandipan0110 <sandipanseal11@gmail.com>
2021-06-26 20:28:10 +0530

    calculating month

--- helloWorld.sh
+++ helloWorld.sh
@@ -4,18 +4,25 @@ echo "Welcome Message"
 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
-empCheck=$((RANDOM%3));
+numWorkingDays=20;
 
-case $empCheck in
-        $isFullTime)
-                empHrs=8
-                ;;
-        $isPartTime)
-                empHrs=8
-                ;;
-        *)
-                empHrs=0
-                ;;
-esac
-salary=$(($empHrs*$empRatePerHr));
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+        empCheck=$((RANDOM%3));
+                case $empCheck in
+                        $isFullTime)
+                                empHrs=8
+                                ;;
+                        $isPartTime)
+                                empHrs=8
+                                ;;
+                        *)
+                                empHrs=0
+                                ;;
+                esac
+                salary=$(($empHrs*$empRatePerHr));
+                totalSalary=$(($totalSalary+$salary));
+done
+
